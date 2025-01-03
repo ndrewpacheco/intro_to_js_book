61a74a6a527cec21184eb694e4768821f8fda7d8
Andrew Pacheco <ndrewpacheco@gmail.com>
2025-01-02 23:14:24 -0500

    fin basics

---
+++ basics/exercises.js
@@ -0,0 +1,98 @@
+// Exercises
+// Concatenate two or more strings, one with your first name and one with your last, to create a string with your full name as its value. For example, if your name is John Doe, think about how you can put 'John' and 'Doe' together to get 'John Doe'.
+// "john" + "doe"
+// Solution
+
+
+// Using arithmetic operators, get the individual digits of a 4-digit number like 4936:
+
+// thousands place is 4
+// hundreds place is 9
+// tens place is 3
+// ones place is 6
+
+
+// let digits = []
+// let multiple = 1
+// {
+// let digit = number % 10;
+// digits.push(digit);
+// number -= (digit)
+// number /= 10
+// multiple *= 10
+// }
+
+
+// number /= 10
+// 4 * 1000 )
+// Solution
+// Identify the data type for each of the following values:
+
+// 'true' toString
+// false boolean
+// 1.5 number
+// 2 bnumber
+// undefined
+// { foo: 'bar' }
+// Solution
+// Explain why this code logs '510' instead of 15.
+
+// Copy Code
+// console.log('5' + 10);
+// Solution
+
+// when a string is being conccatenated with a Number, js coerces the number into a string, and returns a string concating both values .
+// Refactor the code from the previous exercise to use explicit coercion, so it logs 15 instead.
+// console.log(Number('5') + 10);
+// Solution
+// Use the template literal syntax along with the expression Number('5') + 10 to log the following sentence to the console:
+// console.log(`The value of 5 + 10 is ${Number('5') + 10}.`)
+
+
+// Copy Code
+// The value of 5 + 10 is 15.
+// Solution
+// Will an error occur if you try to access an array element with an index that is greater than or equal to the length of the array? For example:
+
+// Copy Code
+// let foo = ['a', 'b', 'c'];
+// console.log(foo.length);  // => 3
+// console.log(foo[3]);      // will this result in an error?
+// Solution
+// Create an array named names that contains a list of pet names. For instance:
+
+// Name
+// asta
+// butterscotch
+// pudding
+// neptune
+// darwin
+// You can use that data or make up your own.
+
+// Solution
+// Create an object named pets that contains a list of pet names and the type of animal. For instance:
+
+// Name	Animal
+// asta	dog
+// butterscotch	cat
+// pudding	cat
+// neptune	fish
+// darwin	lizard
+// You can use that data or make up your own.
+
+// Solution
+// What value does the following expression evaluate to?
+
+// Copy Code
+// 'foo' === 'Foo'
+// Solution
+// What value does the following expression evaluate to?
+
+// Copy Code
+// parseInt('3.1415')
+// Solution
+// What value does the following expression evaluate to?
+
+// Copy Code
+// '12' < '9'
+// Solution
